6b4e3b92011710970f0f45b084bf3bcbd98d803e
Aqueelon <106967535+Aqueelon@users.noreply.github.com>
2023-02-28 15:31:10 +0300

    Забыл добавить return?

--- js/main.mjs
+++ js/main.mjs
@@ -2,7 +2,7 @@ import {createObjectsOfPhoto} from './functions.mjs';
 import {OBJECTS_COUNT} from './const.mjs';
 
 function createPhoto() {
-  Array.from({length: OBJECTS_COUNT}, createObjectsOfPhoto);
+  return Array.from({length: OBJECTS_COUNT}, createObjectsOfPhoto);
 }
 
 createPhoto();
