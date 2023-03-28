ca7705002d68b43188b0f925c6f037ae49ee0ccd
Aqueelon <106967535+Aqueelon@users.noreply.github.com>
2023-03-28 19:58:32 +0300

    Рефакторинг. Исправил баг с радио-инпутом

--- js/upload-form.mjs
+++ js/upload-form.mjs
@@ -1,7 +1,7 @@
 import {isEscapeKey} from './show-big-picture.mjs';
 import {onEditImageFormSubmit, onHashtagInputChange } from './validation-form.mjs';
 import {resetScale, onBiggerButtonClick, onSmallerButtonClick} from './scale-image.mjs';
-import {hideSlider} from './effects.mjs';
+import {hideSlider, resetEffectsSettings} from './effects.mjs';
 
 const modalFormOverlay = document.querySelector('.img-upload__overlay'); // модалка с hidden
 const imageUploadInput = document.querySelector('#upload-file'); //инпут с загрузкой
@@ -9,7 +9,7 @@ const cancelButton = modalFormOverlay.querySelector('.img-upload__cancel');
 const imgUploadForm = document.querySelector('.img-upload__form');
 const hashtagInput = document.querySelector('.text__hashtags');
 const commentField = document.querySelector('.text__description');
-
+const radioDefaultChecked = document.querySelector('#effect-none');
 const buttonControlSmaller = document.querySelector('.scale__control--smaller');
 const buttonControlBigger = document.querySelector('.scale__control--bigger');
 
@@ -19,9 +19,9 @@ const buttonControlBigger = document.querySelector('.scale__control--bigger');
 function openUploadForm() {
   modalFormOverlay.classList.remove('hidden');
   document.querySelector('body').classList.add('modal-open');
+  radioDefaultChecked.checked = true;
   resetScale();
   hideSlider();
-
   cancelButton.addEventListener('click', onCloseUploadForm);
   buttonControlSmaller.addEventListener('click', onSmallerButtonClick);
   buttonControlBigger.addEventListener('click', onBiggerButtonClick);
@@ -39,11 +39,12 @@ function onCloseUploadForm() {
   commentField.value = '';
   modalFormOverlay.classList.add('hidden');
   document.querySelector('body').classList.remove('modal-open');
-
+  resetEffectsSettings();
   cancelButton.removeEventListener('click', onCancelButtonClick);
   buttonControlSmaller.removeEventListener('click', onSmallerButtonClick);
   buttonControlBigger.removeEventListener('click', onBiggerButtonClick);
   document.removeEventListener('keydown', onCancelButtonEscKeydown);
+
 }
 
 /**
