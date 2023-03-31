45ced3c1f38fe1c1f19fc95b31303a663b98d253
Aqueelon <106967535+Aqueelon@users.noreply.github.com>
2023-03-31 20:07:49 +0300

    Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-import {createPhotos} from './mocks/generate-post-data.mjs';
 import {renderPhotos} from './photos-thumbnails.mjs';
-import {OBJECTS_COUNT} from './mocks/const.mjs';
-import {addPictureClickHandler} from './show-big-picture.mjs';
+// import {addPictureClickHandler} from './show-big-picture.mjs';
 import {addPhotoUploadListener} from './upload-form.mjs';
 import {initializeEffects} from './effects.mjs';
 import {validateForm} from './validation-form.mjs';
-const photos = createPhotos(OBJECTS_COUNT);
-renderPhotos(photos);
-addPictureClickHandler(photos);
+import {getData} from './api.mjs';
+// const photos = createPhotos(OBJECTS_COUNT);
+
+// addPictureClickHandler(photos);
 addPhotoUploadListener();
 validateForm();
 initializeEffects();
-
+getData()
+  .then((photosData) => {
+    renderPhotos(photosData);
+  });
--- js/photos-thumbnails.mjs
+++ js/photos-thumbnails.mjs
@@ -2,6 +2,16 @@ const picturesContainer = document.querySelector('.pictures');
 const photoTemplate = document.querySelector('#picture').content;
 
 /**
+ * Удаляем фото посты
+ * @param {DOM Elements} photos
+ */
+function clearPhotoElements(photos) {
+  photos.forEach((photo) => {
+    photo.remove();
+  });
+}
+
+/**
  * Создание элемента фотографии на странице.
  * @param {object} photo - Объект фотографии.
  * @returns {HTMLElement} - Элемент фотографии.
@@ -11,24 +21,29 @@ function createPhotoElement(photo) {
 
   photoComponent.querySelector('a').dataset.id = photo.id;
   photoComponent.querySelector('.picture__img').src = photo.url;
-  photoComponent.querySelector('.picture__img').alt = photo.descriptions;
+  photoComponent.querySelector('.picture__img').alt = photo.description;
   photoComponent.querySelector('.picture__comments').textContent = photo.comments.length;
   photoComponent.querySelector('.picture__likes').textContent = photo.likes;
 
   return photoComponent;
 }
 
+
 /**
  * Отрисовка фотографий на странице.
- * @param {Object} photos - массив объектов с информацией о фотографиях.
+ * @param {Object} photosData - массив объектов с информацией о фотографиях.
  */
-function renderPhotos(photos) {
+function renderPhotos(photosData) {
+  const photos = document.querySelectorAll('.picture');
   const photosFragment = document.createDocumentFragment();
 
-  photos.forEach((photo) => {
+  clearPhotoElements(photos);
+
+  photosData.forEach((photo) => {
     const photoElement = createPhotoElement(photo);
     photosFragment.append(photoElement);
   });
   return picturesContainer.append(photosFragment);
 }
+
 export {renderPhotos, picturesContainer};
